9005299b377edb742885ef4f2bb184b234f4be6b
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: Add API for disabling gestural navigation
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Wed Mar 23 18:40:00 2022 +0530
    
        base: Update API for disabling gestural navigation
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0174ff0e8a30f0ef2b54c1f61145a575b844ad72

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarCommandQueueCallbacks.java
@@ -646,6 +646,13 @@ public class StatusBarCommandQueueCallbacks implements CommandQueue.Callbacks {
     public void toggleCameraFlash() {
         if (mFlashlightController.isAvailable()) {
             mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
+	}
+    }
+
+    @Override
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        if (mStatusBar.getNavigationBarView() != null) {
+            mStatusBar.getNavigationBarView().setBlockedGesturalNavigation(blocked);
         }
     }
 }
