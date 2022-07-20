yaap/frameworks_base_old

SystemUI: QSAnimator: Properly animate bottom brightness slider
The way it used to be the view finished animating before even
showing. Very unpleasant and unnatural.

Change-Id: I8e66ae05b1a327cd3d916cf4e51341064d3d94aa

packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
@@ -393,24 +393,6 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
         }
 
         if (mAllowFancy) {
-            // Make brightness appear static position and alpha in through second half.
-            View brightness = mQsPanelController.getBrightnessView();
-            if (brightness != null) {
-                firstPageBuilder.addFloat(brightness, "translationY",
-                        brightness.getMeasuredHeight() * 0.5f, 0);
-                mBrightnessAnimator = new TouchAnimator.Builder()
-                        .addFloat(brightness, "alpha", 0, 1)
-                        .addFloat(brightness, "sliderScaleY", 0.3f, 1)
-                        .setInterpolator(Interpolators.ALPHA_IN)
-                        .setStartDelay(0.3f)
-                        .build();
-                mAllViews.add(brightness);
-            } else {
-                mBrightnessAnimator = null;
-            }
-            mFirstPageAnimator = firstPageBuilder
-                    .setListener(this)
-                    .build();
             // Fade in the tiles/labels as we reach the final position.
             Builder builder = new Builder()
                     .addFloat(tileLayout, "alpha", 0, 1);
@@ -426,6 +408,35 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
                 // In portrait, media view should always be visible
                 mQsPanelController.mMediaHost.hostView.setAlpha(1.0f);
             }
+
+            // Make brightness appear static position and alpha in through second half.
+            // If brightness is showing at the bottom fade in as we reach the final position
+            View brightness = mQsPanelController.getBrightnessView();
+            if (brightness != null) {
+                boolean bottom = mTunerService.getValue(
+                        QSPanel.QS_BRIGHTNESS_POSITION_BOTTOM, 0) == 1;
+                if (bottom) {
+                    builder.addFloat(brightness, "alpha", 0, 1);
+                    mBrightnessAnimator = null;
+                } else {
+                    firstPageBuilder.addFloat(brightness, "translationY",
+                            brightness.getMeasuredHeight() * 0.5f, 0);
+                    mBrightnessAnimator = new TouchAnimator.Builder()
+                            .addFloat(brightness, "alpha", 0, 1)
+                            .addFloat(brightness, "sliderScaleY", 0.3f, 1)
+                            .setInterpolator(Interpolators.ALPHA_IN)
+                            .setStartDelay(0.3f)
+                            .build();
+                    mAllViews.add(brightness);
+                }
+                mAllViews.add(brightness);
+            } else {
+                mBrightnessAnimator = null;
+            }
+            mFirstPageAnimator = firstPageBuilder
+                    .setListener(this)
+                    .build();
+
             mAllPagesDelayedAnimator = builder.build();
             mAllViews.add(mSecurityFooter.getView());
             translationYBuilder.setInterpolator(mQSExpansionPathInterpolator.getYInterpolator());

AOSPA-sweet/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
@@ -400,11 +400,6 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
         }
 
         if (mAllowFancy) {
-            animateBrightnessSlider(firstPageBuilder);
-
-            mFirstPageAnimator = firstPageBuilder
-                    .setListener(this)
-                    .build();
             // Fade in the tiles/labels as we reach the final position.
             Builder builder = new Builder()
                     .addFloat(tileLayout, "alpha", 0, 1);
@@ -416,7 +411,6 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
                 updateQQSFooterAnimation();
             }
 
-
             // Fade in the security footer and the divider as we reach the final position
             builder = new Builder().setStartDelay(EXPANDED_TILE_DELAY);
             builder.addFloat(mSecurityFooter.getView(), "alpha", 0, 1);
@@ -427,6 +421,20 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
                 // In portrait, media view should always be visible
                 mQsPanelController.mMediaHost.hostView.setAlpha(1.0f);
             }
+
+            View qsBrightness = mQsPanelController.getBrightnessView();
+            final boolean bottom = mTunerService.getValue(
+                    QSPanel.QS_BRIGHTNESS_POSITION_BOTTOM, 0) == 1;
+            if (bottom) {
+                // If brightness is showing at the bottom fade in as we reach the final position
+                builder.addFloat(qsBrightness, "alpha", 0, 1);
+                mBrightnessAnimator = null;
+            }
+            animateBrightnessSlider(firstPageBuilder, bottom);
+            mFirstPageAnimator = firstPageBuilder
+                    .setListener(this)
+                    .build();
+
             mAllPagesDelayedAnimator = builder.build();
             mAllViews.add(mSecurityFooter.getView());
             translationYBuilder.setInterpolator(mQSExpansionPathInterpolator.getYInterpolator());
@@ -452,7 +460,7 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
                 .addFloat(tileLayout, "alpha", 0, 1).build();
     }
 
-    private void animateBrightnessSlider(Builder firstPageBuilder) {
+    private void animateBrightnessSlider(Builder firstPageBuilder, boolean bottom) {
         View qsBrightness = mQsPanelController.getBrightnessView();
         View qqsBrightness = mQuickQSPanelController.getBrightnessView();
         if (qqsBrightness != null && qqsBrightness.getVisibility() == View.VISIBLE) {
@@ -468,14 +476,16 @@ public class QSAnimator implements Callback, PageListener, Listener, OnLayoutCha
                     .addFloat(qqsBrightness, "translationY", 0, translationY)
                     .build();
         } else if (qsBrightness != null) {
-            firstPageBuilder.addFloat(qsBrightness, "translationY",
-                    qsBrightness.getMeasuredHeight() * 0.5f, 0);
-            mBrightnessAnimator = new Builder()
-                    .addFloat(qsBrightness, "alpha", 0, 1)
-                    .addFloat(qsBrightness, "sliderScaleY", 0.3f, 1)
-                    .setInterpolator(Interpolators.ALPHA_IN)
-                    .setStartDelay(0.3f)
-                    .build();
+            if (!bottom) {
+                firstPageBuilder.addFloat(qsBrightness, "translationY",
+                        qsBrightness.getMeasuredHeight() * 0.5f, 0);
+                mBrightnessAnimator = new Builder()
+                        .addFloat(qsBrightness, "alpha", 0, 1)
+                        .addFloat(qsBrightness, "sliderScaleY", 0.3f, 1)
+                        .setInterpolator(Interpolators.ALPHA_IN)
+                        .setStartDelay(0.3f)
+                        .build();
+            }
             mAllViews.add(qsBrightness);
         } else {
             mBrightnessAnimator = null;